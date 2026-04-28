71754b518ca6d732fd49584f4102c9ac8fa7ad11
Weplay <anshbusinessfor@gmail.com>
2026-04-28 17:52:22 +0530

    Update message.dart

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -4,12 +4,24 @@ class Message {
   final String id;
   final String senderId;
   final String text;
+  final String type;
+  final String? giftId;
+  final String? giftName;
+  final String? giftIcon;
+  final int? giftPrice;
+  final int? luckyCoins;
   final Timestamp? createdAt;
 
   Message({
     required this.id,
     required this.senderId,
     required this.text,
+    this.type = 'text',
+    this.giftId,
+    this.giftName,
+    this.giftIcon,
+    this.giftPrice,
+    this.luckyCoins,
     this.createdAt,
   });
 
@@ -19,6 +31,12 @@ class Message {
       id: d.id,
       senderId: m['senderId'] ?? '',
       text: m['text'] ?? '',
+      type: m['type'] ?? 'text',
+      giftId: m['giftId'],
+      giftName: m['giftName'],
+      giftIcon: m['giftIcon'],
+      giftPrice: m['giftPrice'],
+      luckyCoins: m['luckyCoins'],
       createdAt: m['createdAt'],
     );
   }
