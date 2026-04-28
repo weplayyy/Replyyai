d65e35b1dfac7f95a6e1e5e2e8ee34e79b70613c
Weplay <anshbusinessfor@gmail.com>
2026-04-28 16:55:44 +0530

    Add DiscoverTab widget to display FriendsTab

---
+++ lib/screens/tabs/discover_tab.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'friends_tab.dart';
+
+class DiscoverTab extends StatelessWidget {
+  const DiscoverTab({super.key});
+  @override
+  Widget build(BuildContext context) => const FriendsTab();
+}
